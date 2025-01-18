7d83945bb4fd4d48bb65f998a89bca9f45615bd5
Max Nölp <noelpmax@gmail.com>
2025-01-18 22:47:30 +0100

    create views

--- portfolio/landingpage/views.py
+++ portfolio/landingpage/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import ListView
+
 from portfolio.landingpage.models import AboutMeText
+from portfolio.landingpage.models import CodeProjects
+from portfolio.landingpage.models import CodingSkills
 
 
 class AboutMeView(ListView):
@@ -9,6 +11,22 @@ class AboutMeView(ListView):
     context_object_name = "about"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(context["about"])
-        return context
+        return super().get_context_data(**kwargs)
+
+
+class CodingSkillView(ListView):
+    model = CodingSkills
+    template_name = "landingpage/landingpage.html"
+    context_object_name = "skill"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
+class CodeProjects(ListView):
+    model = CodeProjects
+    template_name = "landingpage/landingpage.html"
+    context_object_name = "code"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
